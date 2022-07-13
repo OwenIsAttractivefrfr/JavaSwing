0f09e43be96a0f8cd6c8646d93f33018819ab6dd
Owen <OwenTheAwesome8@gmail.com>
2022-07-13 19:54:47 -0400

    Java Swing Paint Panel

--- src/com/company/PaintPanel.java
+++ src/com/company/PaintPanel.java
@@ -1,6 +1,29 @@
 package com.company;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class PaintPanel
+public class PaintPanel extends JPanel
 {
+    Insets inset;
+    Random randomNumber;
 
+    public PaintPanel()
+    {
+        setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+        randomNumber = new Random();
+    }
+    protected void paintComponent(Graphics graphics)
+    {
+        super.paintComponent(graphics);
+        int x1, x2, y1, y2;
+        int height = getHeight();
+        int width = getWidth();
+        inset = getInsets();
+        for(int i = 0; i < 10; i++)
+        {
+
+        }
+    }
 }
